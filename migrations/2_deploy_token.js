af371f6c7ed6bc6effe857a812280a4eec08e3f7
Veerababu8091 <85879319+Veerababu8091@users.noreply.github.com>
2022-07-07 09:49:23 +0530

    Update 2_deploy_token.js

--- migrations/2_deploy_token.js
+++ migrations/2_deploy_token.js
@@ -1,12 +1,12 @@
-const Token = artifacts.require("./Token");
+const Token = artifacts.require("./Bosi");
 const config = require("../config.json");
 
 const web3 = require('web3');
 const BN = web3.utils.BN;
 
-const initialSupply = new BN(config.initialSupply);
-const name = config.name;
-const symbol = config.symbol;
+const initialSupply = new BN(config.50000000);
+const name = config.Bosi;
+const symbol = config.BNDT;
 const decimals = new BN(config.decimals);
 
 module.exports = function(deployer) {
